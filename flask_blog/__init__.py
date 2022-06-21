42617fcc1277b218cbf28ff39d4f5f5236be348f
OleUp <2987026@mail.ru>
2022-06-21 23:51:04 +0300

    lesson_2

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,20 +7,25 @@ from flask_blog.config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 
 def create_app():
     app = Flask(__name__,
                 static_url_path='',
-                static_folder='../static',
-                template_folder='../templates'
+                static_folder='static',
+                template_folder='templates'
                 )
 
     app.config.from_object(Config)
 
+    db.init_app(app)
+    bcrypt.init_app(app)
     login_manager.init_app(app)
 
     from flask_blog.main.routes import main
+    from flask_blog.users.routes import users
     app.register_blueprint(main)
+    app.register_blueprint(users)
 
     return app
--- flask_blog/models.py
+++ flask_blog/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    avatar = db.Column(db.String(20), nullable=False, default='user_default.png')
+    avatar = db.Column(db.String(20), nullable=True, default='user_default.png')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -29,7 +29,7 @@ class Category(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    data_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
